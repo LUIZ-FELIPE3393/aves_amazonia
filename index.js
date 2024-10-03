f09e953419ff48dc0758f1bc51c9be35767fcb02
unknown <2023003393@ifam.edu.br>
2024-10-02 20:29:25 -0400

    modified:   index.html 	modified:   index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const html = require('html')
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 8989
 
-app.use(express.static('public'))
-app.use('/bootstrap', express.static(__dirname, '/node_modules/'))
+app.use(express.static('./public'))
+app.use('/bootstrap', express.static('./node_modules/bootstrap/dist'))
 
 app.get('/', (req, res) => {
-    res.send()
+    res.sendFile(path.join(__dirname, './index.html'))
 })
 
 app.listen(port, () => {
-    console.log("Servidor aberto na porta " + port)
+    console.log(`O site pode ser acessado em http://localhost:${port}`)
 })
