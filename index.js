9eb9089f38193c8cde9d966f44b44979c2121c15
LUIZ-FELIPE3393 <2023003393@ifam.edu.br>
2024-10-07 16:02:45 -0400

    modified:   index.js 	modified:   package-lock.json 	modified:   package.json 	new file:   public/images/birds/bemtevi/image1.jpg 	new file:   public/images/birds/bemtevi/image2.jpg 	new file:   public/images/birds/bemtevi/image3.jpg 	new file:   public/images/birds/copyright.txt 	new file:   public/images/birds/periquitodacampina/image1.jpg 	new file:   public/images/birds/periquitodacampina/image2.jpg 	new file:   public/images/birds/periquitodacampina/image3.jpg 	new file:   public/js/tinymce-config.js 	modified:   public/output.css 	new file:   src/output.css 	new file:   src/pages/add_bird.html

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const path = require('path')
 const app = express()
 const port = 8989
 
+app.use(express.urlencoded({ extended: false }))
+app.use('/', express.static(path.join(__dirname, 'node_modules', 'tinymce')))
 app.use('/public', express.static('./public'))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './src/index.html'))
+})
+
+app.get('/add-bird-page', (req, res) => {
+    res.sendFile(path.join(__dirname, './src/pages/add_bird.html'))
+})
+
+app.post('/add-bird', (req, res) => {
+    console.log(req.body)
 })
 
 app.listen(port, () => {
