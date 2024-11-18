e7450d779bc51765fb170a769a9284e3e1907652
LUIZ-FELIPE3393 <2023003393@ifam.edu.br>
2024-11-17 21:21:11 -0400

    renamed:    public/images/birds/bird-not-found.jpg -> public/images/bird-not-found.jpg 	modified:   public/js/bird_manager.js 	deleted:    uploads/161482aa4c637c0fe760867ad28a1746 	deleted:    uploads/1ffc61a3c633dd234fb9e4c49e825ded 	deleted:    uploads/23d70a35b4db066dc1640400c5096572 	deleted:    uploads/3cb07132aa07929a9c525cc18fff0af6 	deleted:    uploads/8a20a13efe6f4181f276cc6e441372c8 	deleted:    uploads/a75774a8886d35d117f39f5346b83d40 	deleted:    uploads/b122536f2916ad2d9042a2204996786c

--- public/js/bird_manager.js
+++ public/js/bird_manager.js
@@ -9,16 +9,18 @@ let aveDeleteID = null;
 axios.get('/bird')
     .then(json => {
         for (const bird of json.data) {
-            let thumbnail = bird['thumbnail'];
-            if (thumbnail === null) {
-                thumbnail = "/bird-not-found.jpg"
+            let thumbnail;
+            if (bird['thumbnail'] === null) {
+                thumbnail = "/public/images/bird-not-found.jpg";
+            } else {
+                thumbnail = "/public/images/birds" + bird['thumbnail'];
             }
 
             const birdElement = document.createElement('div')
             birdElement.setAttribute('class', 'flex bg-lime-100 shadow rounded')
             birdElement.setAttribute('bird-id', json['bdt_id'])
             birdElement.innerHTML = prefabs.querySelector("#bird-data").innerHTML
-                .replace(':bdt-image:', '/public/images/birds' + thumbnail)
+                .replace(':bdt-image:', thumbnail)
                 .replace(':bdt-nome:', bird['bdt_nome'])
                 .replace(':bdt-nomecientifico:', bird['bdt_nomecientifico'])
             birdElement.querySelector("#btn-delete").addEventListener('click', (e) => {
