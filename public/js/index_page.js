622850376df3a720eaa61aa75d099d9cfeeccb55
LUIZ-FELIPE3393 <2023003393@ifam.edu.br>
2024-11-27 15:08:13 -0400

    new file:   public/images/avatar.svg 	new file:   public/js/index_page.js 	deleted:    public/js/page_index.js 	modified:   public/output.css 	modified:   src/index.html 	modified:   src/pages/bird_page.html

---
+++ public/js/index_page.js
@@ -0,0 +1,24 @@
+const prefabs = document.querySelector("#prefabs")
+const birdGallery = document.querySelector('#bird-gallery')
+
+// Ler as aves do banco e exibir no grid galeria
+axios.get('bird').then(json => {
+    for (const bird of json.data) {
+        let thumbnail;
+        if (bird['thumbnail'] === null) {
+            thumbnail = "/public/images/bird-not-found.jpg";
+        } else {
+            thumbnail = "/public/images/birds" + bird['thumbnail'];
+        }   
+
+        const birdElement = document.createElement('a')
+        birdElement.setAttribute('class', 'flex bg-lime-100 shadow rounded cursor-pointer')
+        birdElement.setAttribute('bird-id', json['bdt_id'])
+        birdElement.setAttribute('href', `/artigo-ave?id=${bird['bdt_id']}`)
+        birdElement.innerHTML = prefabs.querySelector("#bird-data").innerHTML
+            .replace(':bdt-image:', thumbnail)
+            .replace(':bdt-nome:', bird['bdt_nome'])
+            .replace(':bdt-nomecientifico:', bird['bdt_nomecientifico'])
+        document.querySelector('#bird-gallery').appendChild(birdElement)
+    }
+})
